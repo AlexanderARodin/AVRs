3e17036e9b3d678c98016b70d1c07bc2f7bf5f08
AlexanderARodin <rodin_aa@inbox.ru>
2021-04-12 01:23:33 +0300

    restruct register etc allocation and naming

--- t13a.wdt/main.s
+++ t13a.wdt/main.s
@@ -5,6 +5,7 @@
 .include "../libinc/uartTxMacro.inc"
 .list
 
+
 .CSEG
 .ORG 0x0000
 0x0000:	rjmp RESET			; Reset Handler
@@ -21,44 +22,41 @@
 RESET:
 	; RESET WDT
 	cli
-	in r18,MCUSR			; save RES info
-	ldi r16, 0				; and clear
-	out MCUSR, r16
-	;          76543210
-	ldi r16, 0b00011000		; reset WDT
-	ldi r17, 0b01000111		; reset preScaler
-	out WDTCR, r16
-	out WDTCR, r17
+	in resetInfo, MCUSR			; save RES Info
+	outiMain [MCUSR,0]			; clean RES Info
+	;                  76543210
+	outiMain [WDTCR, (1<<WDCE)|(1<<WDE)]; reset WDT
+	outiMain [WDTCR,(1<<WDTIE)|(1<<WDP2)|(1<<WDP1)|(1<<WDP0)]; reset preScaler
 
 INIT:
-	outi	[SPL, r16, low(RAMEND)]		; Stack
-	outi	[DDRB, r16, 0b00011111]			; output
+	outiMain	[SPL, low(RAMEND)]		; Stack
+	outiMain	[DDRB, (1<<DDB4)|(1<<DDB3)|(1<<DDB2)|(1<<DDB1)|(1<<DDB0)]		; output
 	
-	setTxSilenceUsing [r17]
-	ldi r16, 0x0A
-	rcall typeCharDir
-	ldi r16, 0x0A
-	rcall typeCharDir
-	ldi r16, 'R' 
-	rcall typeCharDir
-	ldi r16, 'E'
-	rcall typeCharDir
-	ldi r16, 'S'
-	rcall typeCharDir
-	ldi r16, '=' 
-	rcall typeCharDir
-		ldi r16,0x30
-		add r16,r18
-		rcall typeCharDir
-	ldi r16, 0x0A
-	rcall typeCharDir
-	ldi r16, 0x0D
-	rcall typeCharDir
+	setTxSilenceUsing
+	ldi argFunc, 0x0A
+	rcall typeChar
+	ldi argFunc, 0x0A
+	rcall typeChar
+	ldi argFunc, 'R' 
+	rcall typeChar
+	ldi argFunc, 'E'
+	rcall typeChar
+	ldi argFunc, 'S'
+	rcall typeChar
+	ldi argFunc, '=' 
+	rcall typeChar
+		ldi argFunc,0x30
+		add argFunc,resetInfo
+		rcall typeChar
+	ldi argFunc, 0x0A
+	rcall typeChar
+	ldi argFunc, 0x0D
+	rcall typeChar
 	
 	; setup timer0
 	;outi	[TCCR0A, r16, 0];b01010010]		; settings for PWM
-	outi	[TCCR0B, r16, 0b00000101]		; pre-scaler 1024
-	outi	[TIMSK0, r16, 0b00000010]		; timer interrupts mask
+	outiMain [TCCR0B, (1<<CS02)|(0<<CS01)|(1<<CS00)]		; pre-scaler 1024
+	outiMain [TIMSK0, (1<<TOIE0)]		; timer interrupts mask
 	;outi	[OCR0A, r16, 200]
 	;outi	[OCR0B, r16, 44]
 	;outi	[GTCCR, r16, 1]
@@ -67,9 +65,9 @@ INIT:
 
 
 MAIN:
-	setTxSilenceUsing [r17]
-	ldi r16, '.' 
-	rcall typeChareSafe
+	setTxSilenceUsing
+	ldi argFunc, '.' 
+	rcall typeChar
 	;in r16,TCNT0
 	;rcall typeChareSafe
 
@@ -84,36 +82,26 @@ MAIN:
 rjmp MAIN
 
 ; -- send char to Tx    --
-typeChareSafe:
-	;cli
-	sendTxAChar [r16,r17]
-	;sei
-	ret
-
-typeCharDir:
-	sendTxAChar [r16,r17]
+typeChar:
+	sendTxAChar
 	ret
 
 
 ; -- waiting subroutine --
-.equ Delay = 3;			; установка константы времени задержки
+.equ Delay = 25			; установка константы времени задержки
 waitLong:
-	ldi R20, Delay * 10
+	ldi tmpMain, Delay * 10
 	rjmp WLoop0
-Wait:
-	LDI  R20, Delay		; загрузка константы для задержки в регистр R17
-WLoop0:
-	LDI  R18, 50		; загружаем число 50 (0x32) в регистр R18
-WLoop1:
-	LDI  R19, 0xC8		; загружаем число 200 (0xC8, $C8) в регистр R19
-WLoop2:
-	DEC  R19			; уменьшаем значение в регистре R19 на 1
-	BRNE WLoop2			; возврат к WLoop2 если значение в R19 не равно 0
-	DEC  R18			; уменьшаем значение в регистре R18 на 1
-	BRNE WLoop1			; возврат к WLoop1 если значение в R18 не равно 0
-	DEC  R20			; уменьшаем значение в регистре R17 на 1
-	BRNE WLoop0			; возврат к WLoop0 если значение в R17 не равно 0
-	RET					; возврат из подпрограммы Wait
+wait:
+	ldi tmpMain, Delay
+	WLoop0:
+		ldi altMain, 0xFF
+		WLoop1:
+		dec altMain
+		brne WLoop1
+	dec tmpMain
+	brne WLoop0
+	ret
 
 
 
@@ -121,73 +109,67 @@ WLoop2:
 ; -- interrupts handlers --
 
 TIM0_OVF:	; Timer0 Overflow Handler
-	pushldi		[r16, 0b00000001]
-	pushSreg	[r0]
-	in r0, PORTB
-	eor r0, r16
-	out PORTB, r0
-	popSreg	[r0]
-	pop r16
+	pushSregIRQ
+	ldi altIRQ, (1<<PB0)
+	in  tmpIRQ, PORTB
+	eor tmpIRQ, altIRQ
+	out PORTB,  tmpIRQ
+	popSregIRQ
 	reti
 
 TIM0_COMPA:	; Timer0 CompareA Handler
-	pushldi		[r16, 0b00000010]
-	pushSreg	[r0]
-	in r0, PORTB
-	eor r0, r16
-	out PORTB, r0
-	popSreg		[r0]
-	pop r16
+	pushSregIRQ
+	ldi altIRQ, (1<<PB1)
+	in  tmpIRQ, PORTB
+	eor tmpIRQ, altIRQ
+	out PORTB,  tmpIRQ
+	popSregIRQ
 	reti
 
 TIM0_COMPB:	; Timer0 CompareB Handler
-	pushldi		[r16, 0b00000100]
-	pushSreg	[r0]
-	in r0, PORTB
-	eor r0, r16
-	out PORTB, r0
-	popSreg 	[r0]
-	pop r16
-	reti
-
-EXT_INT0:	; IRQ0 Handler
-	sbi PORTB, 2
-	reti
-
-PC_INT0:	; PCINT0 Handler
-	sbi PORTB, 2
-	reti
-
-
-EE_RDY:		; EEPROM Ready Handler
-	sbi PORTB, 2
-	reti
-
-ANA_COMP:	; Analog Comparator Handler
-	sbi PORTB, 2
+	pushSregIRQ
+	ldi altIRQ, (1<<PB2)
+	in  tmpIRQ, PORTB
+	eor tmpIRQ, altIRQ
+	out PORTB,  tmpIRQ
+	popSregIRQ
 	reti
 
 WATCHDOG:	; Watchdog Interrupt Handler
-	pushSreg [r16]
-	push r17
-		ldi r16, 'W' 
-		rcall typeCharDir
-		ldi r16, 'D'
-		rcall typeCharDir
-		ldi r16, 'G'
-		rcall typeCharDir
-		ldi r16, '!' 
-		rcall typeCharDir
-		ldi r16, 0x0A
-		rcall typeCharDir
-		ldi r16, 0x0D
-		rcall typeCharDir
-	pop r17
-	popSreg [r16]
+	pushSregIRQ
+	push argFunc
+	push tmpFunc
+		ldi argFunc, 'W' 
+		rcall typeChar
+		ldi argFunc, 'D'
+		rcall typeChar
+		ldi argFunc, 'G'
+		rcall typeChar
+		ldi argFunc, '!' 
+		rcall typeChar
+		ldi argFunc, 0x0A
+		rcall typeChar
+		ldi argFunc, 0x0D
+		rcall typeChar
+	pop tmpFunc
+	pop argFunc
+	popSregIRQ
 	reti
 
+EXT_INT0:	; IRQ0 Handler
+	sbi PORTB, PB3
+	reti
+PC_INT0:	; PCINT0 Handler
+	sbi PORTB, PB3
+	reti
+EE_RDY:		; EEPROM Ready Handler
+	sbi PORTB, PB3
+	reti
+ANA_COMP:	; Analog Comparator Handler
+	sbi PORTB, PB3
+	reti
 ADC_C:		; ADC Conversion Handler
-	sbi PORTB, 2
+	sbi PORTB, PB3
 	reti	; handler exit
 
 ; -- ################### --
