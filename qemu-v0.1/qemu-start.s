78fdb7e3ca05f200e878854309841bee1e15ee56
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-02-21 01:34:58 +0300

    Update qemu-start.s

--- qemu-v0.1/qemu-start.s
+++ qemu-v0.1/qemu-start.s
@@ -4,37 +4,37 @@
 
 .CSEG								; code segment
 .ORG 0x0000						; start address
-0x0000	rjmp RESET			;External Pin, Power-on Reset, Brown-out Reset, and Watchdog Reset
-0x0001	rjmp OTHER_IRQ		;test closure
-0x0002	rjmp OTHER_IRQ		;test closure
-0x0003	rjmp OTHER_IRQ		;test closure
-0x0004	rjmp OTHER_IRQ		;test closure
-0x0005	rjmp OTHER_IRQ		;test closure
-0x0006	rjmp OTHER_IRQ		;test closure
-0x0007	rjmp OTHER_IRQ		;test closure
-0x0008	rjmp OTHER_IRQ		;test closure
-0x0009	rjmp OTHER_IRQ		;test closure
-0x000A	rjmp OTHER_IRQ		;test closure
-0x000B	rjmp OTHER_IRQ		;test closure
-0x000C	rjmp OTHER_IRQ		;test closure
-0x000D	rjmp OTHER_IRQ		;test closure
-0x000E	rjmp OTHER_IRQ		;test closure
-0x000F	rjmp OTHER_IRQ		;test closure
-0x0010	rjmp OTHER_IRQ		;test closure
-0x0011	rjmp OTHER_IRQ		;test closure
-0x0013	rjmp OTHER_IRQ		;test closure
-0x0014	rjmp OTHER_IRQ		;test closure
-0x0015	rjmp OTHER_IRQ		;test closure
-0x0016	rjmp OTHER_IRQ		;test closure
-0x0017	rjmp OTHER_IRQ		;test closure
-0x0018	rjmp OTHER_IRQ		;test closure
-0x0019	rjmp OTHER_IRQ		;test closure
-0x001A	rjmp OTHER_IRQ		;test closure
-0x001B	rjmp OTHER_IRQ		;test closure
-0x001C	rjmp OTHER_IRQ		;test closure
-0x001D	rjmp OTHER_IRQ		;test closure
-0x001E	rjmp OTHER_IRQ		;test closure
-0x001F	rjmp OTHER_IRQ		;test closure
+0x0000:	rjmp RESET			;External Pin, Power-on Reset, Brown-out Reset, and Watchdog Reset
+0x0001:	rjmp OTHER_IRQ		;test closure
+0x0002:	rjmp OTHER_IRQ		;test closure
+0x0003:	rjmp OTHER_IRQ		;test closure
+0x0004:	rjmp OTHER_IRQ		;test closure
+0x0005:	rjmp OTHER_IRQ		;test closure
+0x0006:	rjmp OTHER_IRQ		;test closure
+0x0007:	rjmp OTHER_IRQ		;test closure
+0x0008:	rjmp OTHER_IRQ		;test closure
+0x0009:	rjmp OTHER_IRQ		;test closure
+0x000A:	rjmp OTHER_IRQ		;test closure
+0x000B:	rjmp OTHER_IRQ		;test closure
+0x000C:	rjmp OTHER_IRQ		;test closure
+0x000D:	rjmp OTHER_IRQ		;test closure
+0x000E:	rjmp OTHER_IRQ		;test closure
+0x000F:	rjmp OTHER_IRQ		;test closure
+0x0010:	rjmp OTHER_IRQ		;test closure
+0x0011:	rjmp OTHER_IRQ		;test closure
+0x0013:	rjmp OTHER_IRQ		;test closure
+0x0014:	rjmp OTHER_IRQ		;test closure
+0x0015:	rjmp OTHER_IRQ		;test closure
+0x0016:	rjmp OTHER_IRQ		;test closure
+0x0017:	rjmp OTHER_IRQ		;test closure
+0x0018:	rjmp OTHER_IRQ		;test closure
+0x0019:	rjmp OTHER_IRQ		;test closure
+0x001A:	rjmp OTHER_IRQ		;test closure
+0x001B:	rjmp OTHER_IRQ		;test closure
+0x001C:	rjmp OTHER_IRQ		;test closure
+0x001D:	rjmp OTHER_IRQ		;test closure
+0x001E:	rjmp OTHER_IRQ		;test closure
+0x001F:	rjmp OTHER_IRQ		;test closure
 
 
 RESET:
